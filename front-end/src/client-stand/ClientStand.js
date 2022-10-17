7b5a0d7277e5599059984b825b833159619f0ec3
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-10-17 14:02:32 +0200

    minor changes on ui/ux

--- front-end/src/client-stand/ClientStand.js
+++ front-end/src/client-stand/ClientStand.js
@@ -1,17 +1,17 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import './style/style.css';
+import './style/client-style.css';
 
 const ClientStand = () => {
     return (
-        <Container fluid style={{ minHeight: "100vh", backgroundColor: "lightgreen" }}>
+        <Container fluid style={{ minHeight: "100vh", backgroundColor: "#31A861" }}>
             <Row>
-                <p style={{ fontSize: "40px", textAlign: "center", paddingTop: "20px", fontFamily: "Koblenz" }}>
+                <p className='headerClient' style={{ fontSize: "40px", textAlign: "center", paddingTop: "20px", paddingBottom: "20px", fontFamily: "Koblenz" }}>
                     <b>Please select a service to get enqueued.</b>
                 </p>
             </Row>
-            <Row>
+            <Row style={{marginTop: "100px"}}>
                 <Col className="button">
                     SERVICE NAME WITH AN ICON
                 </Col>
