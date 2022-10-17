13e9ff8ce936d906fdb488fbfa10a0c0c634d87d
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-10-17 14:08:47 +0200

    buttons for client stand selections

--- front-end/src/client-stand/ClientStand.js
+++ front-end/src/client-stand/ClientStand.js
@@ -11,26 +11,73 @@ const ClientStand = () => {
                     <b>Please select a service to get enqueued.</b>
                 </p>
             </Row>
-            <Row style={{marginTop: "100px"}}>
+            <Row style={{ marginTop: "100px" }}>
                 <Col className="button">
-                    SERVICE NAME WITH AN ICON
+                    <Row>
+                        <svg xmlns="http://www.w3.org/2000/svg" style={{marginBottom: "15px"}} width="60" height="60" fill="currentColor" class="bi bi-bandaid" viewBox="0 0 16 16">
+                            <path d="M14.121 1.879a3 3 0 0 0-4.242 0L8.733 3.026l4.261 4.26 1.127-1.165a3 3 0 0 0 0-4.242ZM12.293 8 8.027 3.734 3.738 8.031 8 12.293 12.293 8Zm-5.006 4.994L3.03 8.737 1.879 9.88a3 3 0 0 0 4.241 4.24l.006-.006 1.16-1.121ZM2.679 7.676l6.492-6.504a4 4 0 0 1 5.66 5.653l-1.477 1.529-5.006 5.006-1.523 1.472a4 4 0 0 1-5.653-5.66l.001-.002 1.505-1.492.001-.002Z" />
+                            <path d="M5.56 7.646a.5.5 0 1 1-.706.708.5.5 0 0 1 .707-.708Zm1.415-1.414a.5.5 0 1 1-.707.707.5.5 0 0 1 .707-.707ZM8.39 4.818a.5.5 0 1 1-.708.707.5.5 0 0 1 .707-.707Zm0 5.657a.5.5 0 1 1-.708.707.5.5 0 0 1 .707-.707ZM9.803 9.06a.5.5 0 1 1-.707.708.5.5 0 0 1 .707-.707Zm1.414-1.414a.5.5 0 1 1-.706.708.5.5 0 0 1 .707-.708ZM6.975 9.06a.5.5 0 1 1-.707.708.5.5 0 0 1 .707-.707ZM8.39 7.646a.5.5 0 1 1-.708.708.5.5 0 0 1 .707-.708Zm1.413-1.414a.5.5 0 1 1-.707.707.5.5 0 0 1 .707-.707Z" />
+                        </svg>
+                    </Row>
+                    <Row>
+                        Service Name
+                    </Row>
                 </Col>
                 <Col className="button">
-                    SERVICE NAME WITH AN ICON
+                    <Row>
+                        <svg xmlns="http://www.w3.org/2000/svg" style={{marginBottom: "15px"}} width="60" height="60" fill="currentColor" class="bi bi-battery-charging" viewBox="0 0 16 16">
+                            <path d="M9.585 2.568a.5.5 0 0 1 .226.58L8.677 6.832h1.99a.5.5 0 0 1 .364.843l-5.334 5.667a.5.5 0 0 1-.842-.49L5.99 9.167H4a.5.5 0 0 1-.364-.843l5.333-5.667a.5.5 0 0 1 .616-.09z" />
+                            <path d="M2 4h4.332l-.94 1H2a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h2.38l-.308 1H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2z" />
+                            <path d="M2 6h2.45L2.908 7.639A1.5 1.5 0 0 0 3.313 10H2V6zm8.595-2-.308 1H12a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H9.276l-.942 1H12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.405z" />
+                            <path d="M12 10h-1.783l1.542-1.639c.097-.103.178-.218.241-.34V10zm0-3.354V6h-.646a1.5 1.5 0 0 1 .646.646zM16 8a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8z" />
+                        </svg>
+                    </Row>
+                    <Row>
+                        Service Name
+                    </Row>
                 </Col>
                 <Col className="button">
-                    SERVICE NAME WITH AN ICON
+                    <Row>
+                        <svg xmlns="http://www.w3.org/2000/svg" style={{marginBottom: "15px"}} width="60" height="60" fill="currentColor" class="bi bi-calendar-range" viewBox="0 0 16 16">
+                            <path d="M9 7a1 1 0 0 1 1-1h5v2h-5a1 1 0 0 1-1-1zM1 9h4a1 1 0 0 1 0 2H1V9z" />
+                            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                        </svg>
+                    </Row>
+                    <Row>
+                        Service Name
+                    </Row>
                 </Col>
             </Row>
             <Row>
                 <Col className="button">
-                    SERVICE NAME WITH AN ICON
+                    <Row>
+                        <svg xmlns="http://www.w3.org/2000/svg" style={{marginBottom: "15px"}} width="60" height="60" fill="currentColor" class="bi bi-camera-video" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z" />
+                        </svg>
+                    </Row>
+                    <Row>
+                        Service Name
+                    </Row>
                 </Col>
                 <Col className="button">
-                    SERVICE NAME WITH AN ICON
+                    <Row>
+                        <svg xmlns="http://www.w3.org/2000/svg" style={{marginBottom: "15px"}} width="60" height="60" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                        </svg>
+                    </Row>
+                    <Row>
+                        Service Name
+                    </Row>
                 </Col>
                 <Col className="button">
-                    SERVICE NAME WITH AN ICON
+                    <Row>
+                        <svg xmlns="http://www.w3.org/2000/svg" style={{marginBottom: "15px"}} width="60" height="60" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
+                            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z" />
+                        </svg>
+                    </Row>
+                    <Row>
+                        Service Name
+                    </Row>
                 </Col>
             </Row>
         </Container>
