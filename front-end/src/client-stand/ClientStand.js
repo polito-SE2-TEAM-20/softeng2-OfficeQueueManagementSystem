02905aeb88e8dc9339c9ae39d342decf37af7321
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-10-18 19:07:14 +0200

    integrated: correct icons for service names

--- front-end/src/client-stand/ClientStand.js
+++ front-end/src/client-stand/ClientStand.js
@@ -35,19 +35,34 @@ const ClientStand = () => {
         <Container fluid style={{ minHeight: "100vh", backgroundColor: "#31A861" }}>
             <Row>
                 <p className='headerClient' style={{ fontSize: "40px", textAlign: "center", paddingTop: "20px", paddingBottom: "20px", fontFamily: "Koblenz" }}>
-                    <b>Please select a service to get enqueued.</b>
+                    <b>Please, select a service to get enqueued.</b>
                 </p>
             </Row>
             <Row style={{ marginTop: "100px" }}>
                 {
-                    serviceTypes.map((service) => {
+                    serviceTypes.sort((x, y) => x.code > y.code).map((service) => {
                         return (
                             <Col className="button hoverButton" onClick={() => { onClickWrapper(service.code); soundPlayClass.SoundPlay(audioClip.sound) }}>
                                 <Row>
-                                    <svg xmlns="http://www.w3.org/2000/svg" style={{ marginBottom: "15px" }} width="60" height="60" fill="currentColor" class="bi bi-bandaid" viewBox="0 0 16 16">
-                                        <path d="M14.121 1.879a3 3 0 0 0-4.242 0L8.733 3.026l4.261 4.26 1.127-1.165a3 3 0 0 0 0-4.242ZM12.293 8 8.027 3.734 3.738 8.031 8 12.293 12.293 8Zm-5.006 4.994L3.03 8.737 1.879 9.88a3 3 0 0 0 4.241 4.24l.006-.006 1.16-1.121ZM2.679 7.676l6.492-6.504a4 4 0 0 1 5.66 5.653l-1.477 1.529-5.006 5.006-1.523 1.472a4 4 0 0 1-5.653-5.66l.001-.002 1.505-1.492.001-.002Z" />
-                                        <path d="M5.56 7.646a.5.5 0 1 1-.706.708.5.5 0 0 1 .707-.708Zm1.415-1.414a.5.5 0 1 1-.707.707.5.5 0 0 1 .707-.707ZM8.39 4.818a.5.5 0 1 1-.708.707.5.5 0 0 1 .707-.707Zm0 5.657a.5.5 0 1 1-.708.707.5.5 0 0 1 .707-.707ZM9.803 9.06a.5.5 0 1 1-.707.708.5.5 0 0 1 .707-.707Zm1.414-1.414a.5.5 0 1 1-.706.708.5.5 0 0 1 .707-.708ZM6.975 9.06a.5.5 0 1 1-.707.708.5.5 0 0 1 .707-.707ZM8.39 7.646a.5.5 0 1 1-.708.708.5.5 0 0 1 .707-.708Zm1.413-1.414a.5.5 0 1 1-.707.707.5.5 0 0 1 .707-.707Z" />
-                                    </svg>
+                                    {
+                                        service.code == "A" ?
+                                            (<svg xmlns="http://www.w3.org/2000/svg" style={{ marginBottom: "15px" }} width="60" height="60" fill="currentColor" class="bi bi-bank" viewBox="0 0 16 16">
+                                                <path d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.501.501 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89L8 0ZM3.777 3h8.447L8 1 3.777 3ZM2 6v7h1V6H2Zm2 0v7h2.5V6H4Zm3.5 0v7h1V6h-1Zm2 0v7H12V6H9.5ZM13 6v7h1V6h-1Zm2-1V4H1v1h14Zm-.39 9H1.39l-.25 1h13.72l-.25-1Z" />
+                                            </svg>) : <></>
+                                    }
+                                    {
+                                        service.code == "B" ?
+                                            (<svg xmlns="http://www.w3.org/2000/svg" style={{ marginBottom: "15px" }} width="60" height="60" fill="currentColor" class="bi bi-credit-card" viewBox="0 0 16 16">
+                                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z" />
+                                                <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z" />
+                                            </svg>) : <></>
+                                    }
+                                    {
+                                        service.code == "C" ?
+                                            (<svg xmlns="http://www.w3.org/2000/svg" style={{ marginBottom: "15px" }} width="60" height="60" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
+                                            </svg>) : <></>
+                                    }
                                 </Row>
                                 <Row>
                                     <div style={{ textAlign: "center" }}>{service.name}</div>
