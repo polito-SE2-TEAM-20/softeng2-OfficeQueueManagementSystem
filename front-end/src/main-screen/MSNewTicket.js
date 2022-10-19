086f040b78f5154d2b20bcb6575b4ec5b29c3d17
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-10-19 19:53:15 +0200

    updated: added a waiting mode during fetches

--- front-end/src/main-screen/MSNewTicket.js
+++ front-end/src/main-screen/MSNewTicket.js
@@ -18,8 +18,23 @@ const MSNewTicket = (props) => {
     }, []);
     */
 
-    if(!props.listOfTickets.length) {
-        return null;
+    if (!props.listOfTickets.length) {
+        return (<>
+            <Container>
+                <Row>
+                    <Col>
+                        <div class="sectionTitleLeft header" style={{ marginTop: "140px" }}>Last called ticket</div>
+                    </Col>
+                </Row>
+            </Container>
+            <div class={`newTicketBox center ${newTicketAnimation ? "transparencyAnimation" : ""}`}>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{ color: "white" }} width="80" height="80" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                    </svg>
+                </div>
+            </div>
+        </>);
     }
     return (
         <>
--- front-end/src/main-screen/QueueHistory.js
+++ front-end/src/main-screen/QueueHistory.js
@@ -2,8 +2,23 @@ import Table from 'react-bootstrap/Table'
 
 const QueueHistory = (props) => {
 
-    if(!props.listOfTickets.length) {
-        return null;
+    if (!props.listOfTickets.length) {
+        return (<>
+            <p class="sectionTitleRight" style={{ color: "#ffffff", marginTop: "100px" }}>Queue history</p>
+            <Table style={{ borderCollapse: 'collapse' }}>
+                <thead>
+                    <th className="leftQueueHeader">TICKET ID</th>
+                    <th className="rightQueueHeader">COUNTER</th>
+                </thead>
+                <br></br>
+                <tbody>
+                    <tr>
+                        <td className="leftQueueItemVoid" style={{ backgroundColor: "#228FF5", borderTop: 'none', borderBottom: 'none'}} ><b>Waiting for</b></td>
+                        <td className="rightQueueItemVoid" style={{ backgroundColor: "#0F5EA8", borderTop: 'none', borderBottom: 'none'}} ><b>data...</b></td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>);
     }
     return (
         <>
@@ -23,7 +38,7 @@ const QueueHistory = (props) => {
                                         <td style={{ backgroundColor: "#228FF5", borderTop: 'none', borderBottom: 'none' }} className="leftQueueItem">{ticket.code}</td>
                                         <td style={{ backgroundColor: "#0F5EA8", borderTop: 'none', borderBottom: 'none' }} className="rightQueueItem">{!ticket.counter ? "N/A" : ticket.counter.name}</td>
                                     </tr>
-                                    <br/>
+                                    <br />
                                 </>
                             );
                         })
