428c38c071dfd499978021beaee21d80cf0490de
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-10-19 11:22:16 +0200

    minor changes: ui/ux

--- front-end/src/ticket/ticket.js
+++ front-end/src/ticket/ticket.js
@@ -8,26 +8,24 @@ const Ticket = () => {
     const navigate = useNavigate();
     return (
         <div class="container bootstrap snippets bootdeys">
-<div class="row">
-    <div class="content-card">
-        <div class="card-big-shadow">
-            <div class="card card-just-text" data-background="color" data-color="blue" data-radius="none">
-                <div class="content">
-                    <h6 class="category">TICKET</h6>
-                    <h4 class="title">A0001</h4>
-                    <p class="description">Expetcted time:</p>
-                    <p class="description">15 minutes</p>
-
-          <Button variant="outline-secondary" onClick={()=> navigate('/clientstand')}>Close</Button>
-                    
+            <div class="row">
+                <div class="content-card">
+                    <div class="card-big-shadow">
+                        <div class="card card-just-text" data-background="color" data-color="blue" data-radius="none">
+                            <div class="content">
+                                <h6 class="category center" style={{fontFamily: "Akira"}}>TICKET</h6>
+                                <h4 class="title" style={{color: "black", fontFamily: "Akira", fontSize: "36px", marginBottom: "48px"}}><b>A0001</b></h4>
+                                <p class="description" style={{marginTop: "24px"}}>Expetcted time:</p>
+                                <p class="description" style={{fontSize: "25px"}}><b>15 minutes</b></p>
+                                <Button style={{marginTop: "24px"}} variant="outline-secondary" onClick={() => navigate('/clientstand')}>Close</Button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div> 
+            </div>
         </div>
-    </div>
-    </div>
-    </div>
-    
-    
+
+
     );
 }
 
