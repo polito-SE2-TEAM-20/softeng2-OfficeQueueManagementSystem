069b4f8918d685070ff7a37e998cc549bc1c408b
German Gorodnev <germangorodnev@gmail.com>
2022-10-19 18:46:00 +0200

    try five

--- back-end/src/main-screen/main-screen.service.ts
+++ back-end/src/main-screen/main-screen.service.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 
-import { Ticket } from '../entities';
+import { Counter, Ticket } from '../entities';
 
 @Injectable()
 export class MainScreenService {
   constructor(private dataSource: DataSource) {}
 
   async retrieveLastFive() {
-    const last = await this.dataSource
+    const tickets = await this.dataSource
       .getRepository(Ticket)
       .createQueryBuilder('ticket')
+      .leftJoinAndMapOne(
+        'ticket.counter',
+        Counter,
+        'c',
+        'c.id = ticket.counterId',
+      )
       .where('ticket.state = 1')
       .orderBy('ticket.servedAt', 'DESC')
       .limit(5)
       .getMany();
 
-    const conc = last.map(t => t.serviceCode + t.position);
+    const conc = tickets.map(t => t.serviceCode + t.position);
 
     return conc;
   }
