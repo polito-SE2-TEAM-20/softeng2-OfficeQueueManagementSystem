3ffb49bae35561806076f57e24391873502bc25b
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-10-20 11:41:10 +0200

    fixed: management page height

--- front-end/src/Management/managementpage.js
+++ front-end/src/Management/managementpage.js
@@ -3,7 +3,7 @@ import { LoginButton } from '../Login/Login';
 
 const Managementpage = () => {
     return (
-        <Container fluid style={{ minHeight: "80vh", backgroundColor: "#31A861" }}>
+        <Container fluid style={{ minHeight: "100vh", backgroundColor: "#31A861" }}>
             <Row>
                 <p className='headerClient' style={{ fontSize: "40px", textAlign: "center", paddingTop: "20px", paddingBottom: "20px", fontFamily: "Koblenz" }}>
                     <b>Management</b>
